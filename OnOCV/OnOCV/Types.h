392d3dd61d7bc014e9c5f0012a369042f781388b
TankleL <Tankle_@hotmail.com>
2015-11-22 21:48:20 +0800

    new header file

---
+++ OnOCV/OnOCV/Types.h
@@ -0,0 +1,20 @@
+///	@file		Funcs.h
+///	@brief		...
+///	@licence	...
+///	@designer	Tankle L.
+///	@date		First created at < November 22th, 2015 >
+///
+///	@version	0.1.0
+///
+///	@programmer	Tankle L.
+///	@programmer ...
+
+
+#ifndef _TYPES_H
+#define	_TYPES_H
+
+
+
+
+
+#endif // !_TYPES_H
