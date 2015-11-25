fccd3868154a3bb2f6b7c5c8fad9315ea9eb7a9a
TankleL <Tankle_@hotmail.com>
2015-11-25 12:54:22 +0800

    fixed bug - gradient algorithm: upside down before

--- OnOCV/OnOCV/OnOCV.cpp
+++ OnOCV/OnOCV/OnOCV.cpp
@@ -13,52 +13,17 @@ LARGE_INTEGER	g_perfCountOld;
 LARGE_INTEGER	g_perfCountNew;
 
 #define	TIMETOUCH(Counter)	QueryPerformanceCounter(&Counter)
-#define	PRINTTIME(TEXT)		{ cout << TEXT << ",\t#" << (((double)g_perfCountNew.QuadPart) - ((double)g_perfCountOld.QuadPart)) / (double)g_perfFeq.QuadPart * 1000 << "ms" << endl; }
+#define	PRINTTIME(Text)		{ cout << Text << ",\t#" << (((double)g_perfCountNew.QuadPart) - ((double)g_perfCountOld.QuadPart)) / (double)g_perfFeq.QuadPart * 1000 << "ms" << endl; }
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	QueryPerformanceFrequency(&g_perfFeq);
+	Mat	img = imread("..\\..\\ImgRes\\t2.png");
 
-	TIMETOUCH(g_perfCountOld);
-	Mat		imgL = imread("..\\..\\ImgRes\\Small_L_S0.jpg");
-	TIMETOUCH(g_perfCountNew);
-	PRINTTIME("Load Left Image");
+	RELATIVEPXGRADIENT_3x3*	pGra = nullptr;
+	int nGra = CalculateRelativePxGradient3x3(pGra, img);
 
-	TIMETOUCH(g_perfCountOld);
-	Mat		imgR = imread("..\\..\\ImgRes\\Small_R_S0.jpg");
-	TIMETOUCH(g_perfCountNew);
-	PRINTTIME("Load Right Image");
-
-
-	RELATIVEPXGRADIENT_5x5*	pGradientsL = nullptr;
-	RELATIVEPXGRADIENT_5x5*	pGradientsR = nullptr;
-
-	TIMETOUCH(g_perfCountOld);
-	int nGradientsL = CalculateRelativePxGradient5x5(pGradientsL, imgL);
-	TIMETOUCH(g_perfCountNew);
-	PRINTTIME("Analyzed Left Image");
-
-
-	TIMETOUCH(g_perfCountOld);
-	int nGradientsR = CalculateRelativePxGradient5x5(pGradientsR, imgR);
-	TIMETOUCH(g_perfCountNew);
-	PRINTTIME("Analyzed Right Image");
-
-
-	TIMETOUCH(g_perfCountOld);
-	OutputAsTxt_PxGradient5x5("..\\..\\FireField\\aloeL_01_Gradient5x5.txt", pGradientsL, nGradientsL);
-	TIMETOUCH(g_perfCountNew);
-	PRINTTIME("Exported Left Image");
-
-	TIMETOUCH(g_perfCountOld);
-	OutputAsTxt_PxGradient5x5("..\\..\\FireField\\aloeR_01_Gradient5x5.txt", pGradientsR, nGradientsR);
-	TIMETOUCH(g_perfCountNew);
-	PRINTTIME("Exported Right Image");
-
-	SAFE_DELETE_ARRAY(pGradientsL);
-	SAFE_DELETE_ARRAY(pGradientsR);
-
+	OutputAsTxt_PxGradient3x3("..\\..\\FireField\\t2_3x3.txt", pGra, nGra);
 
 	return 0;
 }
